9768e6f15d41e4bc2b532032ee4b56a6124d8424
Thibaud EHRET <thibaud.ehret@ens-cachan.fr>
2023-06-06 13:22:15 +0200

    Add an option to remove cloudy images from the set of candidate images

--- tsd/get.py
+++ tsd/get.py
@@ -4,8 +4,15 @@ import geojson
 import shapely
 import datetime
 import os
-from tsd import utils
+import rasterio
+from tsd import utils, parallel
+from tsd import s2_metadata_parser
 from tsd import get_sentinel2, get_sentinel1, get_sentinel3
+
+# TODO improve mirror and api selection and other pre-defined parameters
+# TODO factorize the two functions
+# TODO documentation
+# TODO Fix everything so that it works with other satellites (S1 and S3)
 
 spatial_resolution = {'Sentinel2': (10, 10), 'Sentinel1': (10, 3), 'Sentinel3': (500, 500)}
 get_sat = {'Sentinel2': get_sentinel2, 'Sentinel1': get_sentinel1, 'Sentinel3': get_sentinel3}
@@ -29,6 +36,26 @@ def get_series(args):
 
     # list available images
     images = get.search(args.geom, args.start_date, args.end_date)
+
+    # Removes images that are too cloudy before downloading anything
+    if args.filter_cloudy is not None:
+        # Retrieve SCL images that contains a cloud mask. FIXME use the GML directly since it less costly but hidden behind a request payer for now
+        get.download(images, ['SCL'], args.geom, out_dir=args.outdir)
+        # Check which image is cloudy
+        def cloud_coverage(img):
+            fname = os.path.join(args.outdir, '{}_band_SCL.tif'.format(img.filename))
+            try:
+                with rasterio.open(fname) as src:
+                    clouds = src.read()
+                    profile = src.profile
+                    clouds = ((clouds == 3).sum() + (clouds == 9).sum()) / (clouds.shape[1] * clouds.shape[2])
+            except rasterio.errors.RasterioIOError:
+                print("WARNING: download of {} failed".format(fname))
+                clouds = 1.1 # Guarantee discarding the image
+            return clouds
+
+        cloudy = [cloud_coverage(img) > args.filter_cloudy for img in images]
+        images = [im for im, cloud in zip(images, cloudy) if not cloud]
 
     # Only keep a limited number of images because of IPOL time constraints
     if args.max > 0:
@@ -61,6 +88,26 @@ def get_nearest(args):
 
     # list available images
     images = get.search(args.geom, args.start_date, args.end_date)
+    # Removes images that are too cloudy before downloading anything
+    if args.filter_cloudy is not None:
+        # Retrieve SCL images that contains a cloud mask. FIXME use the GML directly since it less costly but hidden behind a request payer for now
+        get.download(images, ['SCL'], args.geom, out_dir=args.outdir)
+        # Check which image is cloudy
+        def cloud_coverage(img):
+            fname = os.path.join(args.outdir, '{}_band_SCL.tif'.format(img.filename))
+            try:
+                with rasterio.open(fname) as src:
+                    clouds = src.read()
+                    profile = src.profile
+                    clouds = ((clouds == 3).sum() + (clouds == 9).sum()) / (clouds.shape[1] * clouds.shape[2])
+            except rasterio.errors.RasterioIOError:
+                print("WARNING: download of {} failed".format(fname))
+                clouds = 1.1 # Guarantee discarding the image
+            return clouds
+
+        cloudy = [cloud_coverage(img) > args.filter_cloudy for img in images]
+        images = [im for im, cloud in zip(images, cloudy) if not cloud]
+
     # select the nearest date
     images = [min(images, key=lambda x: abs(x.date.date() - args.date.date()))]
 
@@ -97,6 +144,7 @@ def main():
     parser_series.add_argument('-o', '--outdir', type=str, help=('path to save the '
                                                                  'images'), default='')
     parser_series.add_argument('-m', '--max', type=int, help='maximum amount of images to be retrieved', default=-1)
+    parser_series.add_argument('-f', '--filter-cloudy', type=float, help='Pre-filter images that are more cloudy than this fraction', default=None)
     parser_series.set_defaults(func=get_series) 
 
 
@@ -116,6 +164,7 @@ def main():
                                      ' download. No check are performed here')
     parser_nearest.add_argument('-o', '--outdir', type=str, help=('path to save the '
                                                                  'images'), default='')
+    parser_nearest.add_argument('-f', '--filter-cloudy', type=float, help='Pre-filter images that are more cloudy than this fraction', default=None)
     parser_nearest.set_defaults(func=get_nearest) 
 
     args = parser.parse_args()
